b93ccb7db9a886307a4afaf2b4ba632837043480
Shubh596 <pandeyaashu4@gmail.com>
2022-05-29 15:43:09 +0530

    star

---
+++ index.js
@@ -0,0 +1,42 @@
+let id;
+
+async function getData() {
+    let search = document.getElementById("search").value;
+
+    let url = `https://swapi.dev/api/people/?search=${search}`;
+
+    let res = await fetch(url);
+
+    let data = await res.json();
+
+    console.log(data);
+
+    return data.results;
+}
+
+function append(data) {
+    let container = document.getElementById("results");
+    container.innerHTML = null;
+
+    data.forEach(function (el) {
+        let p = document.createElement("p");
+        p.innerText = el.name;
+
+        container.append(p);
+    });
+}
+
+async function main() {
+    let data = await getData();
+
+    append(data);
+}
+
+function debouncing(func, delay) {
+    if (id) {
+        clearTimeout(id);
+    }
+    id = setTimeout(function () {
+        func();
+    }, delay);
+}
